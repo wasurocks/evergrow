09d402b23dc1878b40c5d382032b3e51db104a0d
GreenColoredLife <greencoloredlife@gmail.com>
2020-12-11 01:24:17 +0700

    feat: Login/Register/Home pages + nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: "app-root",
+    templateUrl: "app.component.html",
+    styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+    navigate: any;
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.sideMenu();
+        this.initializeApp();
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
+
+    sideMenu() {
+        this.navigate = [
+            {
+                title: "Overview",
+                url: "/home",
+                icon: "albums-outline",
+            },
+            {
+                title: "Set Budget",
+                url: "/budget",
+                icon: "wallet-outline",
+            },
+            {
+                title: "Analysis",
+                url: "/analysis",
+                icon: "stats-chart-outline",
+            },
+        ];
+    }
 }
